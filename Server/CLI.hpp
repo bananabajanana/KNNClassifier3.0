7615a88ace8065898cf716d7171aeb8eb8d15203
bananabajanana <ohad.heines@gmail.com>
2022-08-26 16:51:29 +0300

    Added a todo list

--- Server/CLI.hpp
+++ Server/CLI.hpp
@@ -4,6 +4,17 @@
 #include <vector>
 #include "Server/Commands/Command.hpp"
 
+/**
+ * TODO:
+ *      - Generalize old classifier
+ *          - work with general string instead of enum.
+ *          - input function needs to figure out nth rank.
+ *          - work with 3 vectors.
+ *      - Implement Commands
+ *      - Implement IO Services
+ *      - Implement Command Line Interface
+ */
+
 class CLI {
 private:
     std::vector<Command> commands;
