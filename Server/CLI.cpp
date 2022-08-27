422eca0fe83f336a35847b5861e68ca06b3084f8
bananabajanana <ohad.heines@gmail.com>
2022-08-27 12:27:09 +0300

    Added exit function to CLI

--- Server/CLI.cpp
+++ Server/CLI.cpp
@@ -3,14 +3,25 @@
 void CLI::start() {
     while(true) {
         std::string menu = "Welcome to the KNN Classifier Server. Please chose an option:\n";
-        for(int i = 0; i < commands.size(); i++) {
+        for(int i = 1; i <= commands.size(); i++) {
             menu += i + ". ";
             menu += commands[i].getDescription() + "\n";
         }
+        menu += (commands.size()+1) + ". exit\n";
+
         dio->write(menu);
         std::string decision = dio->read();
         try {
-            commands[std::stoi(decision)];
+            int numDecision = std::stoi(decision) - 1;
+            if (numDecision == commands.size()) {
+                //exit the server/close the connection
+                return;
+            }
+            if(numDecision < 0 || numDecision > commands.size()) {
+                commands[numDecision].execute();
+            } else {
+                dio->write("Please chose a valid option.");
+            }
         } catch(std::exception& e) {
             dio->write("Please select an option by writing a number.");
         }
