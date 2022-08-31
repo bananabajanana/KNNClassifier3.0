16c3f405252911e7ddd59f3a64521b231207aada
bananabajanana <ohad.heines@gmail.com>
2022-08-31 14:53:43 +0300

    major bugfixes : buffer size now 4096

--- Server/IOServices/SocketIO.cpp
+++ Server/IOServices/SocketIO.cpp
@@ -17,25 +17,39 @@ bool hasChar(char *arr, int n, char c) {
 }
 
 std::string SocketIO::read() {
-    std::string output = "";
-    char buffer[1024]={0};
-    int read_bytes = recv(socket, buffer, expected_data_len, 0);
-    if (read_bytes <= 0) {
-        server.deleteSocket(socket);
-        return "7";
-    }
-    output += buffer;
-    return output;
-}
-
-void SocketIO::write(std::string message) {
-    char* toSend = const_cast<char *>(message.c_str());
-    int message_len = strlen(toSend);
-    int sent_bytes = send(socket, toSend, message_len, 0);
+    //region SendingBacklog
+    toSend += '\003';
+    char* toSendArr = const_cast<char *>(toSend.c_str());
+    int message_len = strlen(toSendArr);
+    int sent_bytes = send(socket, toSendArr, message_len, 0);
 
     if (sent_bytes < 0) {
         perror("error sending to client");
     }
+    toSend = "";
+    //endregion
+
+    //region Reading new Message
+    std::string output = "";
+    char buffer[4096]={0};
+    do {
+        int read_bytes = recv(socket, buffer, expected_data_len, 0);
+        if (read_bytes <= 0) {
+            server.deleteSocket(socket);
+            return "7";
+        }
+        output += buffer;
+    } while(output[output.length() - 1] != '\003');
+    output.pop_back();
+    return output;
+    //endregion
 }
 
-SocketIO::SocketIO(int socket, ServerProcess &s) :socket(socket), server(s) {}
+void SocketIO::write(std::string message) {
+    toSend += message;
+
+}
+
+SocketIO::SocketIO(int socket, ServerProcess &s) :socket(socket), server(s) {
+    toSend = "";
+}
